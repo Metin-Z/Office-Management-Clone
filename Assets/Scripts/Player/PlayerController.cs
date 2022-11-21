41770a3d9c6af2ae2bb29a679d137f6f309db36d
Metin-Z <metinzehiroglu34@gmail.com>
2022-11-21 12:50:03 +0300

    Ground / Player Control

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    public DynamicJoystick dynamicJoystick;
+    public float speed;
+
+    #region SerializeFields
+    [SerializeField] private Animator anim;
+    #endregion
+
+    public virtual void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+   
+    private void Update()
+    {
+       // if (!GameManager.instance.isGameRunning)
+        //    return;
+        JoyStickMovement();
+    }
+    public void JoyStickMovement()
+    {
+        
+            Vector3 moveForward = new Vector3(dynamicJoystick.Horizontal, 0, dynamicJoystick.Vertical);
+            transform.Translate(speed * Time.fixedDeltaTime * moveForward, Space.World);
+            moveForward.Normalize();
+            if (dynamicJoystick.Horizontal != 0 || dynamicJoystick.Vertical != 0)
+            {
+                transform.forward = moveForward;
+                anim.SetBool("Walk", true);
+            }
+            else
+            {
+                anim.SetBool("Walk", false);
+            }
+        
+    }
+}
